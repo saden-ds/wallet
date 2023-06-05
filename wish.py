e6ec26d5b970322184fef9dd0b4a3771eca2dce2
DannZolik <60573142+DannZolik@users.noreply.github.com>
2023-06-05 10:42:01 +0300

    Update wish.py

--- wish.py
+++ wish.py
@@ -1,22 +1,26 @@
 from product import Product
 from datetime import datetime
-
 class Wish:
     def __init__(self, name:str, date:str, products:list[Product]) -> None:
         self.name = name
         self.fullprice = 0
-        for product in products:
-            self.fullprice += product.price*product.count
+        
         if len(date) == 0:
             self.date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         else:
             self.date = date
+
         self.products = products
+
+        self.calculate_fullprice()
+
+    def calculate_fullprice(self):
+        for product in self.products:
+            self.fullprice += product.price * product.count
 
     # получение json структуры всех продуктов в желании (без json.dumps -> обычный dict).
     def get_data_products(self):
         return [product.get_data_product() for product in self.products]
-
     # получение json структуры всего желания (без json.dumps -> обычный dict).
     def get_data_wish(self):
         data = {
